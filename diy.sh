f10261f0ebbcc5793f630a40f82191c4318fa87d
digitcloud <edwardcai0710@gmail.com>
2020-04-13 01:04:17 +0800

    compiled 4/13 mod2

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
 # Add theme from rosywrt
-git clone https://github.com/rosywrt/luci-theme-rosy.git lienol/lienol
+git clone https://github.com/rosywrt/luci-theme-rosy.git feeds/lienol/lienol
 
 # Deassociate bootstrap as default theme
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
