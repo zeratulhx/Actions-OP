be6264e82219cdc36ca4fa91a77a57913cc891c9
digitcloud <edwardcai0710@gmail.com>
2020-04-13 00:01:55 +0800

    compiled 4/13

--- diy.sh
+++ diy.sh
@@ -6,4 +6,14 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+
+# Add theme from rosywrt
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-atmaterial
+
+# Deassociate bootstrap as default theme
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# Delete default password
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
