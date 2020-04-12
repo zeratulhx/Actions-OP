82038eb5b0f3965f95e1ff698112848ced8ba50e
digitcloud <edwardcai0710@gmail.com>
2020-04-13 01:41:31 +0800

    compiled 4/13 mod3

--- diy.sh
+++ diy.sh
@@ -9,11 +9,11 @@
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
 # Add theme from rosywrt
-git clone https://github.com/rosywrt/luci-theme-rosy.git feeds/lienol/lienol
+git clone https://github.com/rosywrt/luci-theme-rosy.git feeds/lienol/lienol/luci-theme-rosy
 
 # Deassociate bootstrap as default theme
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # Delete default password
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-settings
 
