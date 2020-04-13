5562ac6ee0938858ba416a6aab79157f66f2687e
edwardcai <edwardcai0710@gmail.com>
2020-04-13 18:02:23 +0800

    fix rosy-theme untacked

--- diy.sh
+++ diy.sh
@@ -9,7 +9,8 @@
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
 # Add theme from rosywrt
-git clone https://github.com/rosywrt/luci-theme-rosy.git feeds/lienol/lienol/luci-theme-rosy
+git clone https://github.com/rosywrt/luci-theme-rosy.git feeds/lienol/lienol/luci-theme-rosy-dir
+mv feeds/lienol/lienol/luci-theme-rosy-dir/luci-theme-rosy feeds/lienol/lienol/luci-theme-rosy && rm -rf feeds/lienol/lienol/luci-theme-rosy-dir
 
 # Deassociate bootstrap as default theme
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
