87251f710255dcea8f813a957f109821bbb96c94
digitcloud <edwardcai0710@gmail.com>
2020-04-19 04:42:59 +0800

    compiled 4/19

---
+++ third_party.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: digitcloud
+#=================================================
+cd openwrt
+
+echo 'src-git rosy https://github.com/rosywrt/luci-theme-rosy' >> feeds.conf.default
