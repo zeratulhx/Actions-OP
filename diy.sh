ef8c09dc54515acd55496f53235e8f09d4bba9f9
digitcloud <edwardcai0710@gmail.com>
2020-08-30 01:59:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -14,6 +14,6 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-settings
 
 # Change sipmle-obfs package to lean's source
-cd feeds/lienol/package/
+cd feeds/diy1/package/
 rm -rf simple-obfs
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs
