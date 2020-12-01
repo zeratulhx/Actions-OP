a1753bf0b6f9b166e8887d6deb128cd230a125e3
digitcloud <edwardcai0710@gmail.com>
2020-12-01 23:05:51 +0800

    compiled 12/1

--- diy.sh
+++ diy.sh
@@ -7,9 +7,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
-# Deassociate bootstrap as default theme
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
 # Delete default password
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-settings
 
