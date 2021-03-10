bfd53b9805a0431f5cd5f4a97c45f23f37238995
digitcloud <edwardcai0710@gmail.com>
2021-03-10 18:46:40 +0800

    remove change on passwd

--- diy.sh
+++ diy.sh
@@ -7,6 +7,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
-# Delete default password
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-settings
 
