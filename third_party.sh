7509652fb99aaa7a80127efd218641161d907dc8
digitcloud <edwardcai0710@gmail.com>
2021-04-26 01:43:45 +0800

    bump to Luci 18.06

--- third_party.sh
+++ third_party.sh
@@ -9,3 +9,6 @@
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 sed -i '$a src-git jerrykuku https://github.com/jerrykuku/openwrt-package.git' feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+
+# Use luci 18.06 on lienol feed
+sed -i 's/openwrt-luci.git;17.01/openwrt-luci.git;18.06/g' feeds.conf.default
